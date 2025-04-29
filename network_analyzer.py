9bc58989d064cee6f888e31e139bd16a6cc39855
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-04-29 06:17:47 +0000

    illya

--- network_analyzer.py
+++ network_analyzer.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import os 
+import os
+import csv
 
 dir_name = "csv"
 os.listdir(dir_name)
@@ -15,8 +16,10 @@ print("\nCSV files:")
 for i in range(len(files)):
     print(f"{i + 1}. {files[i]}")
 choice = int(input("\nEnter file you want to load: ")) - 1
+file_path = os.path.join(dir_name, files[choice])
 
-with open('Giants.csv', mode ='r')as file:
+with open(file_path, mode ='r') as file:
   csvFile = csv.reader(file)
   for lines in csvFile:
         print(lines)
+
