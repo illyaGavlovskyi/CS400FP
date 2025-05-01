98eb257d93448b86d342d45dcd1e51ba84e130be
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-05-01 00:35:16 +0000

    illya

--- network_analyzer.py
+++ network_analyzer.py
@@ -18,8 +18,18 @@ for i in range(len(files)):
 choice = int(input("\nEnter file you want to load: ")) - 1
 file_path = os.path.join(dir_name, files[choice])
 
-with open(file_path, mode ='r') as file:
-  csvFile = csv.reader(file)
-  for lines in csvFile:
-        print(lines)
+#with open(file_path, mode ='r') as file:
+#  csvFile = csv.reader(file)
+#  for lines in csvFile:
+#        print(lines)
+
+df = pd.read_csv(file_path)
+dr = pd.read_csv('devices.csv')
+# print(df.to_string())
+source_counts = df['Source'].value_counts()
+destination_counts = df['Destination'].value_counts()
+print(source_counts)
+print(destination_counts)
+print(dr)
+
 
