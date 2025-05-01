4e1808bb4f179a47d930381a1f6559f30d067003
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-05-01 01:50:14 +0000

    illya

--- network_analyzer.py
+++ network_analyzer.py
@@ -1,7 +1,4 @@
-#pandass  for data analysis
-import pandas as pd 
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pandas as pd
 import numpy as np
 import os
 import csv
@@ -18,18 +15,10 @@ for i in range(len(files)):
 choice = int(input("\nEnter file you want to load: ")) - 1
 file_path = os.path.join(dir_name, files[choice])
 
-#with open(file_path, mode ='r') as file:
-#  csvFile = csv.reader(file)
-#  for lines in csvFile:
-#        print(lines)
-
 df = pd.read_csv(file_path)
 dr = pd.read_csv('devices.csv')
-# print(df.to_string())
 source_counts = df['Source'].value_counts()
 destination_counts = df['Destination'].value_counts()
 print(source_counts)
 print(destination_counts)
 print(dr)
-
-
